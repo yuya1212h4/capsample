003c1d481b99fd96a84f83e66eda497d2289dedd
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-10-29 15:46:20 +0900

    change pid socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,12 +3,14 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 
 working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/var/run/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 # listen 3000
-listen "/tmp/unicorn.sock"
+# listen "/tmp/unicorn.sock"
+listen "/var/run/unicorn.sock"
 timeout 60
 
 preload_app true
