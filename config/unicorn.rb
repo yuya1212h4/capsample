3c25bd2873f4d91aba03f689a3b88328c33175bf
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-10-29 15:52:11 +0900

    fix socket location

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ worker_processes 1
 
 working_directory app_path
 # pid "#{app_path}/tmp/pids/unicorn.pid"
-pid "#{app_path}/var/run/unicorn.pid"
+pid "/var/run/unicorn.pid"
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
